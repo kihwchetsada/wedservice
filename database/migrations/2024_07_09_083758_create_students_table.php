3baba17b6e7e539bf1dc4e98a8d020bafb61599d
chetsadaphon <114697178+kihwchetsada@users.noreply.github.com>
2024-07-09 15:48:55 +0700

    update

--- database/migrations/2024_07_09_083758_create_students_table.php
+++ database/migrations/2024_07_09_083758_create_students_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('birthday');
+            $table->string('department');
+            $table->float('weight');
+            $table->ipAddress('ip_adress');
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
