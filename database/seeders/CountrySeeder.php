03f9836183661bcea20452d0a91d605eece2bf96
chetsadaphon <114697178+kihwchetsada@users.noreply.github.com>
2024-07-16 15:06:01 +0700

    update

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+
 
 class CountrySeeder extends Seeder
 {
